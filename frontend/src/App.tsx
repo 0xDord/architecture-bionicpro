267c51aaaa7d4ec7ddb9061be836eb07a8c75901
anaumov <roflsham@mail.ru>
2025-10-19 20:37:45 +0300

    implemented PKCE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,14 @@ const keycloak = new Keycloak(keycloakConfig);
 
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider 
+      authClient={keycloak} 
+      initOptions={{ 
+        pkceMethod: 'S256',
+        flow: 'standard',
+        responseMode: 'query'
+      }}
+    >
       <div className="App">
         <ReportPage />
       </div>
